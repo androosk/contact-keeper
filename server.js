1c09e453d637eef757aeededad1daedd87331cb3
androosk <atirpok3@gmail.com>
2019-07-21 11:55:54 -0400

    why not

--- server.js
+++ server.js
@@ -10,16 +10,12 @@ connectDB()
 // Init middleware
 app.use(express.json({ extended: false }))
 
-app.get("/", (req,res) =>
-  res.json({ msg: "Welcome to the Contact Keeper API" })
-)
-
 // Routes
 app.use("/api/users", require("./routes/users"))
 app.use("/api/contacts", require("./routes/contacts"))
 app.use("/api/auth", require("./routes/auth"))
 
-// Server static assets in production
+// Serve static assets in production
 if(process.env.NODE_ENV === "production") {
   // Set static folder
   app.use(express.static("client/build"))
@@ -28,4 +24,4 @@ if(process.env.NODE_ENV === "production") {
 
 const PORT = process.env.PORT || 5000
 
-app.listen(PORT, () => console.log(`API server started on port ${PORT}`))
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
